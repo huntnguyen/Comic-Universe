0de64f5bcb8aedf5e193075d2c4ddc8fcaeb4857
MominAsif <mominasif213@gmail.com>
2023-11-07 13:09:50 -0600

    2nd commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,19 +11,20 @@ const HomeScreen = function(props){
 
   const searchApi = async function(name){
     try{
-        axios 
-            .get("https://superheroapi.com/api/" + 
-             "API KEY" +
-                "/search/ironman")
-            .then((response) => { 
-                console.log(response.data)
-            }); 
-      // const response = await API.get('/character', {
-      //   params: {
-      //   }
-      // });
-      // setResults(response.results);
-      // console.log(response.results);
+        //axios 
+           // .get("https://superheroapi.com/api/" + 
+             //"API KEY" +
+                //"/search/ironman")
+           // .then((response) => { 
+              //  console.log(response.data)
+           // }); 
+       const response = await API.get('http://comicvine.gamespot.com/api/characters/?api_key=3a531bfb45a299035190b4b0860d42972ddd2de8& filter=name: X-men&field_list=name' ,{
+         params: {
+          format: "json",
+         }
+       });
+       setResults(response.results);
+       console.log(response);
     }
     catch(e){
       console.log(e, "error");
