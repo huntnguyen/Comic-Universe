fc5de6c0a69de40edb9cb2493010f44f9318199d
madogeri <119082125+madogeri@users.noreply.github.com>
2023-11-14 13:31:34 -0600

    login and create account screens

---
+++ src/screens/CreateAccountScreen.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity, ImageBackground, Image, TextInput} from "react-native";
+import bg from '../../assets/comicbg.gif'
+import Images from "../components/Images";
+
+//mike: testing for file update
+
+const CreateAccountScreen = (props) => {
+
+  return (
+    <View style = {styles.root}>
+
+      <ImageBackground
+        style = {styles.container}
+        imageStyle={styles.image}
+        source = {bg}
+        resizeMode = 'cover'
+      >
+        <Text style={styles.text}>Create{"\n"} Account</Text>
+        <Images style = {styles.image2} imageSource={require('../../assets/superhero.png')}/>
+        <View style={styles.button1}>
+        <TextInput placeholder="Enter New Username"/>
+        </View>
+        <View style={styles.button1}>
+        <TextInput placeholder="Enter New Password"/>
+        </View>
+        <View style={styles.button}>
+        <Button 
+          title = {"Create an Account"} 
+          color="gold"
+          onPress={() => props.navigation.navigate('HomeScreen')}
+        />
+        </View>
+        
+      </ImageBackground>
+    </View>
+  );
+  
+};
+
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  },
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  image: {
+    opacity: .5
+  },
+  text: {
+    textAlign: 'center', 
+    fontSize: 55, 
+    fontWeight: 'bold', 
+    color: 'yellow',
+    textShadowColor: 'tan', textShadowRadius: 30,
+    marginBottom: 20
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: 'yellow',
+    elevation: 3,
+    backgroundColor: 'blue',
+    color: 'red',
+    width: 400,
+    height: 100,
+    marginBottom: 10
+  },
+  image2: {
+    marginBottom: 20,
+  },
+  button1: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: 'yellow',
+    elevation: 3,
+    backgroundColor: '#fff',
+    color: 'red',
+    width: 400,
+    height: 100,
+    marginBottom: 10,
+    color: 'red'
+  },
+});
+
+export default CreateAccountScreen;
